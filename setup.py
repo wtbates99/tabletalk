8a4a20ef182ea16c0b27ec96253d388a766fbe0f
wtbates99 <wtbates99@gmail.com>
2025-03-12 13:55:36 -0400

    working

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     python_requires=">=3.9",
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "License :: CC BY-NC-SA 4.0",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
